055a5c3bc0d252f0a57144ce5ef36629f0272229
Murilo Maldonado Mendes <murilomaldonadom@gmail.com>
2023-10-22 12:46:46 -0300

    feat: Aula 2

--- src/main/java/br/com/murilo3m/todolist/user/UserController.java
+++ src/main/java/br/com/murilo3m/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.murilo3m.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var hasUser = this.userRepository.findByUsername(userModel.getUsername());
+        if(hasUser != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
--- src/main/java/br/com/murilo3m/todolist/user/UserModel.java
+++ src/main/java/br/com/murilo3m/todolist/user/UserModel.java
@@ -1,7 +1,26 @@
 package br.com.murilo3m.todolist.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Entity(name="tb_users")
 public class UserModel {
-    public String username;
-    public String name;
-    public String password;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique=true)
+    private String username;
+    private String name;
+    private String password;
+    @CreationTimestamp
+    private LocalDate createdAt;
 }
